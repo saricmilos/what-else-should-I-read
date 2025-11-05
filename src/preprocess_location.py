4eed4975d177ebd6f382ceedcc24271d20888d34
saricmilos <milossaric@outlook.com>
2025-11-05 17:26:59 +0100

    Lower cased names for consistent naming

--- src/preprocess_location.py
+++ src/preprocess_location.py
@@ -1,40 +1,40 @@
 import pandas as pd
 
 def split_location(df):
-    location_split = df['Location'].str.split(',', expand=True)
-    df['City'] = location_split[0].str.strip()
-    df['State'] = location_split[1].str.strip()
-    df['Country'] = location_split[2].str.strip()
-    df[['City', 'State', 'Country']] = df[['City', 'State', 'Country']].fillna('Unknown')
+    location_split = df['location'].str.split(',', expand=True)
+    df['city'] = location_split[0].str.strip()
+    df['state'] = location_split[1].str.strip()
+    df['country'] = location_split[2].str.strip()
+    df[['city', 'state', 'country']] = df[['city', 'state', 'country']].fillna('unknown')
     return df
 
 def clean_country(df, country_mapping, region_mapping):
-    df['Country'] = df['Country'].str.lower().str.strip()
-    df['Country'] = df['Country'].map(lambda x: country_mapping.get(x, x))
-    counts = df['Country'].value_counts()
+    df['country'] = df['country'].str.lower().str.strip()
+    df['country'] = df['country'].map(lambda x: country_mapping.get(x, x))
+    counts = df['country'].value_counts()
     rare_countries = counts[counts <= 10].index
-    df['Country_Clean'] = df['Country'].apply(lambda x: 'Other' if x in rare_countries else x)
-    df['Country_Clean'] = df['Country_Clean'].str.strip().str.replace('"', '')
-    df['Country_Clean'] = df['Country_Clean'].replace('', 'unknown')
-    df['Region'] = df['Country_Clean'].map(lambda x: region_mapping.get(x, 'Other'))
-    df = df.drop(columns=['Country'])
+    df['country_clean'] = df['country'].apply(lambda x: 'other' if x in rare_countries else x)
+    df['country_clean'] = df['country_clean'].str.strip().str.replace('"', '')
+    df['country_clean'] = df['country_clean'].replace('', 'unknown')
+    df['Region'] = df['country_clean'].map(lambda x: region_mapping.get(x, 'other'))
+    df = df.drop(columns=['country'])
     return df
 
 def clean_city(df, top_n=50):
-    df['City_Clean'] = df['City'].str.strip().str.lower()
-    df['City_Clean'] = df['City_Clean'].replace({'': 'unknown', 'n/a': 'unknown'})
-    city_counts = df['City_Clean'].value_counts()
+    df['city_clean'] = df['city'].str.strip().str.lower()
+    df['city_clean'] = df['city_clean'].replace({'': 'unknown', 'n/a': 'unknown'})
+    city_counts = df['city_clean'].value_counts()
     top_cities = city_counts.nlargest(top_n).index
-    df['City_Clean'] = df['City_Clean'].apply(lambda x: x if x in top_cities else 'Other')
-    df = df.drop(columns=['City'])
+    df['city_clean'] = df['city_clean'].apply(lambda x: x if x in top_cities else 'other')
+    df = df.drop(columns=['city'])
     return df
 
 def clean_state(df, top_n=50):
-    df['State_Clean'] = df['State'].str.strip().str.lower()
-    df['State_Clean'] = df['State_Clean'].replace(['', 'n/a', '\\n/a"', 'na'], 'Other')
-    top_states = df['State_Clean'].value_counts().nlargest(top_n).index
-    df['State_Clean'] = df['State_Clean'].apply(lambda x: x if x in top_states else 'Other')
-    df = df.drop(columns=['State'])
+    df['state_clean'] = df['state'].str.strip().str.lower()
+    df['state_clean'] = df['state_clean'].replace(['', 'n/a', '\\n/a"', 'na'], 'other')
+    top_states = df['state_clean'].value_counts().nlargest(top_n).index
+    df['state_clean'] = df['state_clean'].apply(lambda x: x if x in top_states else 'other')
+    df = df.drop(columns=['state'])
     return df
 
 def preprocess_location(df, country_mapping, region_mapping, top_cities=50, top_states=50):
